ea68ca0e0db79114ef39faf0944c9d562a1851b6
mrTwice <yampolskiy86@gmail.com>
2025-08-23 12:33:23 +0300

    fix after checkstyle

--- hw03-spring-boot/src/main/java/ru/otus/hw/config/AppProperties.java
+++ hw03-spring-boot/src/main/java/ru/otus/hw/config/AppProperties.java
@@ -2,7 +2,6 @@ package ru.otus.hw.config;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.Locale;
--- hw03-spring-boot/src/main/java/ru/otus/hw/service/LocalizedIOServiceImpl.java
+++ hw03-spring-boot/src/main/java/ru/otus/hw/service/LocalizedIOServiceImpl.java
@@ -1,7 +1,6 @@
 package ru.otus.hw.service;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
