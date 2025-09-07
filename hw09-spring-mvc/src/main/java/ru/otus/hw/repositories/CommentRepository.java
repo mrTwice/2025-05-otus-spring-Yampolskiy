2444e8dc2816a4b8611900ae930795b2e059093c
mrTwice <yampolskiy86@gmail.com>
2025-09-07 10:52:36 +0300

    add template for hw9

---
+++ hw09-spring-mvc/src/main/java/ru/otus/hw/repositories/CommentRepository.java
@@ -0,0 +1,13 @@
+package ru.otus.hw.repositories;
+
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.otus.hw.models.Comment;
+
+import java.util.List;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    @EntityGraph(attributePaths = {"book"})
+    List<Comment> findByBookIdOrderByCreatedAtDesc(Long bookId);
+}
---
+++ hw09-spring-mvc/src/test/java/ru/otus/hw/repositories/GenreRepositoryTest.java
@@ -0,0 +1,41 @@
+package ru.otus.hw.repositories;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import ru.otus.hw.models.Genre;
+
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+class GenreRepositoryTest {
+
+    @Autowired
+    TestEntityManager tem;
+
+    @Autowired
+    GenreRepository genreRepository;
+
+    @Test
+    void findAll_returnsAllGenres() {
+        var g1 = tem.persistFlushFind(new Genre(null, "G1"));
+        var g2 = tem.persistFlushFind(new Genre(null, "G2"));
+
+        var all = genreRepository.findAll();
+        assertThat(all).extracting(Genre::getName).contains("G1", "G2");
+    }
+
+    @Test
+    void findAllByIds_returnsSubset() {
+        var g1 = tem.persistFlushFind(new Genre(null, "G1"));
+        var g2 = tem.persistFlushFind(new Genre(null, "G2"));
+        tem.flush();
+
+        var subset = genreRepository.findByIdIn(Set.of(g1.getId()));
+        assertThat(subset).hasSize(1);
+        assertThat(subset.get(0).getId()).isEqualTo(g1.getId());
+    }
+}
